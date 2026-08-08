2e85342968938ff06f2fe1cee025e85b8f5e4d45
calcagnocarloalberto1-star <calcagnocarloalberto1@gmail.com>
2026-08-08 21:39:12 +0200

    Remove X/Twitter link from footer
    Removed X/Twitter social link due to inactive profile.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -97,12 +97,7 @@ export default function Footer() {
             bgColor="#FF0000"
             icon={<svg viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4"><path d="M23.498 6.186a3.016 3.016 0 00-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 00.502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 002.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 002.122-2.136C24 15.93 24 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>}
           />
-          <SocialLink
-            href="https://x.com"
-            label="X"
-            bgColor="#000000"
-            icon={<svg viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4"><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932L18.901 1.153zM17.61 20.644h2.039L6.486 3.24H4.298L17.61 20.644z"/></svg>}
-          />
+          {/* DATA-03: link X/Twitter rimosso — nessun profilo attivo da collegare */}
         </div>
 
         {/* Bottom bar */}
